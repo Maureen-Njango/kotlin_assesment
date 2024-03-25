277860b1ced88aec98186a4f568b1cedd0cf4c74
Maueen-Njango <maureennjangombira@gmail.com>
2024-03-25 08:35:04 +0300

    add files

---
+++ src/Main.kt
@@ -0,0 +1,25 @@
+fun main(){
+    println(data(String))
+    numbers((arrayOf(12, 16,13,46)))
+
+
+}
+fun data = ("barnie bakes brown bagals and burns")
+  return(data)
+
+   fun numbers = (num:Int, num2:Int, num3:Int, num4:Int){
+       return (numbers)
+    println(numbers.add)
+    println(numbers.sort.contentToString)
+    println(numbers.average)
+
+
+   }
+fun raduis = 30
+fun
+
+
+
+
+
+
